2eacf86e4d59241b271826860038d2beffd7aeed
Guilherme Garcia <pectinidae@gmail.com>
2014-07-07 19:44:33 -0300

    added BuildSigma.R

---
+++ BuildSigma.R
@@ -0,0 +1,15 @@
+BuildSigma <- function (matrix.array)
+    ### constructs Sigma matrix (second-order tensor representation of a fourth-order covariance
+    ### tensor between symmetric matrices), following Hine et al., 2009
+    {
+        variances <- aaply (matrix.array, 3, diag)
+        covariances <- aaply (matrix.array, 3, function (x) x [lower.tri (x)])
+        block.var <- var (variances)
+        block.off <- cov (variances, covariances)
+        block.cov <- var (covariances)
+        ### return (list (dim (block.var), dim (block.off), dim (block.cov)))
+        upper.block <- cbind (block.var, sqrt (2) * block.off)
+        lower.block <- cbind (sqrt (2) * t (block.off), 2 * block.cov)
+        Sigma <- rbind (upper.block, lower.block)
+        return (Sigma)
+    }
