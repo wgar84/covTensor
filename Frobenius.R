954141bfa20542ddbf5a165d16cc7e270c6603db
Guilherme Garcia <pectinidae@gmail.com>
2014-07-07 16:59:10 -0300

    added Frobenius.R

---
+++ Frobenius.R
@@ -0,0 +1,11 @@
+FrobDistance <- function (A, B)
+    ### Computes Frobenius distance between two matrices
+    {
+        difference <- A - B
+        return (sqrt (sum (diag (t (difference) %*% difference))))
+    }
+
+FrobNorm <- function (A)
+    ### Computes Frobenius norm of a matrix
+    sqrt (sum (diag (t (A) %*% A)))
+    
