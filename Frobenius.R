a3d42e606e4f1dfc2f31db818426c92642a241c2
Guilherme Garcia <wgar@usp.br>
2014-07-08 03:57:30 -0300

    FrobInner and FrobCos added to Frobenius.R

--- Frobenius.R
+++ Frobenius.R
@@ -9,3 +9,10 @@ FrobNorm <- function (A)
     ### Computes Frobenius norm of a matrix
     sqrt (sum (diag (t (A) %*% A)))
     
+FrobInner <- function (A, B)
+  ### Computes Frobenius inner product between two matrices
+  sum (diag (t (A) %*% B))
+
+FrobCos <- function (A, B)
+  ### Computes cosine of the angle between two matrices
+  FrobInner (A, B) / FrobNorm (A) * FrobNorm (B)
