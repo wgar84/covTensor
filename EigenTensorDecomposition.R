ed4d2889812d0ec8894077e63a4f1696323a86e4
Guilherme Garcia <pectinidae@gmail.com>
2014-07-10 14:53:24 -0300

    removed RiemDist.R and removed FrobDist

--- EigenTensorDecomposition.R
+++ EigenTensorDecomposition.R
@@ -1,4 +1,5 @@
-EigenTensorDecomposition <- function (matrix.array)
+EigenTensorDecomposition <-
+    function (matrix.array)
     ### performs eigentensor decomposition on a set of matrices in array form,
     ### following Basser \& Pajevic, 2007
     {
@@ -8,14 +9,16 @@ EigenTensorDecomposition <- function (matrix.array)
         n.tensor <- min (n.matrix, dim (Sigma) [1])
         eigen.dec <- eigen (Sigma)
         eigen.matrices <-
-            aaply (eigen.dec $ vectors [, 1:n.tensor], 2, ### only 
+            aaply (eigen.dec $ vectors [, 1:n.tensor], 2, ### ematrices with non-zero evals
                    function (x)
                    {
                        eigen.mat <- diag (x [1:n.traits])
-                       eigen.mat [upper.tri (eigen.mat)] <- x [(n.traits+1):length (x)] / sqrt (2)
+                       eigen.mat [upper.tri (eigen.mat)] <-
+                           x [(n.traits+1):length (x)] / sqrt (2)
                        eigen.mat <- eigen.mat + t(eigen.mat)
                        diag (eigen.mat) <- diag (eigen.mat) / 2
                        eigen.mat
                    })
-        return (list ('values' = eigen.dec $ values [1:n.tensor], 'matrices' = eigen.matrices))
+        return (list ('values' = eigen.dec $ values [1:n.tensor],
+                      'matrices' = eigen.matrices))
     }
--- RiemDist.R
+++
@@ -1,4 +0,0 @@
-RiemDist <- function (A, B)
-    ### Computes Riemannian distance between two matrices, following Moakher, 2005.
-    sqrt (sum (log (eigen (solve (A) %*% B) $ values) ^ 2))
-    
